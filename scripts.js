0bef3c7649ed3a20862c2f48e81b7cbcb51e7fe1
IrateLizard20 <jbbusiness2007@gmail.com>
2024-11-17 21:36:04 -0500

    upload

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function Flags(){
+    window.open("./flags/","_self")
+}
+function Kins(){
+    window.open("./kins/","_self")
+}
+function Selfships(){
+    window.open("./selfships/","_self")
+}
+function Back(){
+    window.open("/hub/","_self")
+}
+
+function ErraticGender(){
+    window.open('https://rentry.co/erraticgender')
+}
