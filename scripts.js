780c4c6129387d46e649082aa309359b9f0434e8
IrateLizard20 <jbbusiness2007@gmail.com>
2024-11-29 20:18:50 -0500

    update

--- scripts.js
+++ scripts.js
@@ -21,3 +21,17 @@ function Box(){
 function Razor(){
     window.open('https://toyhou.se/30358965.selfship-art/30359749.razor/gallery')
 }
+
+function loadCSS(href){
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = href;
+    document.head.appendChild(link);
+}
+if (typeof screen.orientation !== 'undefined') {
+    loadCSS('style.css')
+}
+else {
+    loadCSS('style-mobile.css')
+}
